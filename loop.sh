8d730cdc06634e58d237b7f369d5e26137e5fd08
Michael Kreil <github@michael-kreil.de>
2022-04-21 10:17:45 +0200

    add loop

---
+++ loop.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd ${0%/*};
+
+while true; do
+	git pull
+
+	node ./bin/1_scrape.js
+	node ./bin/2_check.js
+	node ./bin/3_analyze.js
+
+	git add ./data
+	git commit -m "automatic update"
+	git push
+
+	sleep 12h
+done
